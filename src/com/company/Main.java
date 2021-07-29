43a72709180c1e8b920aa83bf5234c535d7c6e6f
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-07-28 18:36:22 -0700

    v 1.1 just adds way more fluff. Changes from trident to sword.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,7 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Fireball;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -19,12 +17,15 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.entity.ProjectileHitEvent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main extends JavaPlugin implements Listener {
+
+    Map<String, Long> cooldowns = new HashMap<String, Long>();
 
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
@@ -36,7 +37,7 @@ public class Main extends JavaPlugin implements Listener {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (label.equalsIgnoreCase("poseidon")) {
+        if (label.equalsIgnoreCase("doom")) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage("Players only BOSS");
                 return true;
@@ -45,11 +46,11 @@ public class Main extends JavaPlugin implements Listener {
             if (player.getInventory().firstEmpty() == -1) {
                 Location loc = player.getLocation();
                 World world = player.getWorld();
-                world.dropItemNaturally(loc, getPoseiden());
+                world.dropItemNaturally(loc, getDoom());
                 player.sendMessage(ChatColor.GOLD + "da floor");
                 return true;
             }
-            player.getInventory().addItem(getPoseiden());
+            player.getInventory().addItem(getDoom());
             player.sendMessage(ChatColor.GOLD + "Blessed is he!");
         }
         if (label.equalsIgnoreCase("sugma")) {
@@ -60,21 +61,23 @@ public class Main extends JavaPlugin implements Listener {
         }
         return false;
     }
-    public ItemStack getPoseiden() {
-        ItemStack poseiden = new ItemStack(Material.TRIDENT);
-        ItemMeta meta = poseiden.getItemMeta();
-        meta.setDisplayName(ChatColor.GOLD + "God Poke");
+    public ItemStack getDoom() {
+        ItemStack doom = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta meta = doom.getItemMeta();
+        meta.setDisplayName(ChatColor.BLACK + "Sword O' Doom!");
         List<String> lore = new ArrayList<String>();
         lore.add("");
-        lore.add(ChatColor.ITALIC + "" + ChatColor.GREEN + "Trident O' Poseiden");
+        lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_RED + "+10 dmg dealt against pregnant women");
+        lore.add("");
+        lore.add(ChatColor.DARK_PURPLE + "recharging skelly spawns take 25 seconds");
         meta.setLore(lore);
         meta.addEnchant(Enchantment.LURE, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         meta.setUnbreakable(true);
-        poseiden.setItemMeta(meta);
+        doom.setItemMeta(meta);
 
-        return poseiden;
+        return doom;
     }
 
     public ItemStack pissPants() {
@@ -96,20 +99,32 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler()
     public void onClick(PlayerInteractEvent event) {
-        if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.TRIDENT))
+        if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.NETHERITE_SWORD))
             if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta()
-                    .getDisplayName().contains("God Poke")) {
+                    .getDisplayName().contains("Sword O' Doom!")) {
                 if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasLore()) {
                     Player player = (Player) event.getPlayer();
                     if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                        if (cooldowns.containsKey(player.getName())) {
+                            if (cooldowns.get(player.getName()) > System.currentTimeMillis()) {
+                                return;
+                            }
+                        }
+                            cooldowns.put(player.getName(), System.currentTimeMillis() + 25 * 1000);
+
                         Block b = player.getTargetBlock(null, 5);
                         Location loc = b.getLocation();
                         loc.setY(loc.getY() + 1);
-                        loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
-                        loc.setX(loc.getX() + 1);
-                        loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
-                        loc.setX(loc.getX() - 2);
-                        loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
+                        int duck = 0;
+                        while (3 != duck) {
+                            loc.setX(loc.getX() + 1);
+                            Entity sdrown = loc.getWorld().spawnEntity(loc, EntityType.WITHER_SKELETON);
+                            sdrown.setCustomName("Skelly O' Doom!");
+                            sdrown.setCustomNameVisible(true);
+                            sdrown.setGlowing(true);
+                            duck += 1;
+                        }
+
                     }
                     if (event.getAction() == Action.LEFT_CLICK_AIR) {
                         player.launchProjectile(Fireball.class);
